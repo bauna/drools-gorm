0fea2c2e14a4cd7a9f7f1e3955ad8fac4930de28
bauna <bauna@bauna.com.ar>
2010-11-02 13:34:23 -0300

    initial commit

---
+++ src/groovy/ar/com/bauga/drools/gorm/Pepe.groovy
@@ -0,0 +1,9 @@
+package ar.com.bauga.drools.gorm
+
+import org.drools.event.process.ProcessEvent
+
+class Pepe {
+	def getPepe() {
+		ProcessEvent p = null;
+	}
+}
---
+++ src/java/ar/com/bauga/drools/gorm/Juan.java
@@ -0,0 +1,9 @@
+package ar.com.bauga.drools.gorm;
+
+import org.drools.event.process.ProcessEvent;
+
+public class Juan {
+	public void pepe() {
+		ProcessEvent p = null;
+	}
+}
