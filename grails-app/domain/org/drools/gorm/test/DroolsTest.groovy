a640d62573986e574a980fef5ef0c9ccf07957ca
bauna <bauna@bauna.com.ar>
2010-11-12 19:32:18 -0300

    test domain class

---
+++ grails-app/domain/org/drools/gorm/test/DroolsTest.groovy
@@ -0,0 +1,16 @@
+package org.drools.gorm.test
+
+class DroolsTest implements Serializable {
+
+	String name
+	Long value
+	Date dateCreated
+	Date lastUpdated
+	
+    static constraints = {
+		name(maxSize:64, unique:true)
+		value(blank:true)
+		dateCreated(display:false)
+		lastUpdated(display:false)
+	}
+}
