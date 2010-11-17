51b60c240d08b368854607c2b3b871f40737b44d
bauna <bauna@bauna.com.ar>
2010-11-17 13:20:09 -0300

    changed to allow Serializables id

--- src/groovy/org/drools/gorm/marshalling/GrailsPlaceholderResolverStrategy.groovy
+++ src/groovy/org/drools/gorm/marshalling/GrailsPlaceholderResolverStrategy.groovy
@@ -3,6 +3,7 @@ package org.drools.gorm.marshalling
 import java.io.IOException
 import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
+import java.io.Serializable;
 import java.util.HashMap
 import java.util.IdentityHashMap
 import java.util.Map
@@ -27,14 +28,14 @@ public class GrailsPlaceholderResolverStrategy
                                                        ClassNotFoundException {
     	String domain_name = os.readObject()
     	Object domain = this.grailsApplication.getClassForName(domain_name)
-    	Long id = os.readLong()
+    	Serializable id = os.readObject()
         return domain.get(id)
     }
 
     public void write(ObjectOutputStream os,
                       Object object) throws IOException {
         os.writeObject( object.class.name )
-        os.writeLong((Long) object.id)
+        os.writeObject((Serializable) object.id)
     }
 
     public boolean accept(Object object) {
