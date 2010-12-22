6168ab4c30a5e46fc819921ebc35413482159412
Pablo Nussembaum <baunax@gmail.com>
2010-12-22 16:33:26 -0300

    indentation

--- src/java/org/drools/gorm/session/SessionInfo.java
+++ src/java/org/drools/gorm/session/SessionInfo.java
@@ -5,13 +5,15 @@ import java.util.Date;
 import org.drools.gorm.session.marshalling.GormSessionMarshallingHelper;
 
 public interface SessionInfo {
+    
+    String SAFE_GORM_COMMIT_STATE = "__gormCanCommit";
 
 	int getId();
 
 	void setMarshallingHelper(GormSessionMarshallingHelper helper);
 
 	byte[] getData();
-
+	
 	Date getStartDate();
 
 	Date getLastModificationDate();
