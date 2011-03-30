1ffbddf06c6b339fc6c23730fc86379d22bdfe4b
Pablo Nussembaum <baunax@gmail.com>
2011-03-30 14:16:04 -0300

    update version

--- DroolsGormGrailsPlugin.groovy
+++ DroolsGormGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class DroolsGormGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.5.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.4 > *"
     // the other plugins this plugin depends on
