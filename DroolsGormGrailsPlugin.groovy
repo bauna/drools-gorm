e7e855eaa11386fcf23250ec59604cc612975815
Pablo Nussembaum <baunax@gmail.com>
2011-09-12 18:43:50 -0300

    increase version

--- DroolsGormGrailsPlugin.groovy
+++ DroolsGormGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import grails.util.Environment;
 
 class DroolsGormGrailsPlugin {
     // the plugin version
-    def version = "0.5.3"
+    def version = "0.5.4"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.4 > *"
     // the other plugins this plugin depends on
